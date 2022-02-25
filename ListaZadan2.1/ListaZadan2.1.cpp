b3c3045f76d1c1e37716cca75005a1941c68f6ac
Boberi <53124732+Boberi@users.noreply.github.com>
2022-02-25 17:07:11 +0100

    Create ListaZadan2.1.cpp

---
+++ ListaZadan2.1/ListaZadan2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void write(unsigned n)
+{
+	for (unsigned i = 0; i < n; i++)
+	{
+		cout << "Roman Tosakov" << endl;
+	}
+}
+
+
+int main()
+{
+	unsigned n;
+	cout << "Write number of lines: ";
+	cin >> n;
+	write(n);
+}
